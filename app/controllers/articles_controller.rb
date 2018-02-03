fd9b15ad42b022d021d4ed8589a013f3ce32fd9e
xylennn <himanshu@harshal.in>
2018-02-04 00:23:11 +0530

    this is styled

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to @article,flash:{ success: 'Article was successfully created.' }}
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.html { redirect_to @article,flash:{success: 'Article was successfully updated.'} }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url,flash:{ success: 'Article was successfully destroyed.' }}
       format.json { head :no_content }
     end
   end
